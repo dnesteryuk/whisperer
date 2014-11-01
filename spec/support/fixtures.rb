3ba58b1254b3dbdd06d09c1951eed831f643180f
Dmitriy Nesteryuk <nesterukd@gmail.com>
2014-11-01 13:26:27 +0200

    fix the issue with launching tests on different versions of Ruby

--- spec/support/fixtures.rb
+++ spec/support/fixtures.rb
@@ -1,6 +1,8 @@
 module Fixtures
   # Returns content of a generated fixture
   def fixture(fixture_name)
-    File.read('spec/fixtures/' << fixture_name << '.yml')
+    dir = RUBY_VERSION =~ /2.1.[1-9]/ ? '2.1.x/' : '2.0.x/'
+
+    File.read('spec/fixtures/' << dir << fixture_name << '.yml')
   end
 end
