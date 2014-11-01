fee820b7aa5cb7af4cf1273aec15d5981bbc2d28
Dmitriy Nesteryuk <nesterukd@gmail.com>
2014-11-01 14:54:55 +0200

    fix the issue with passign tests on 2.0.x and 2.1.x

--- spec/support/fixtures.rb
+++ spec/support/fixtures.rb
@@ -1,8 +1,16 @@
 module Fixtures
   # Returns content of a generated fixture
   def fixture(fixture_name)
-    dir = RUBY_VERSION =~ /2.1.[1-9]/ ? '2.1.x/' : '2.0.x/'
-
-    File.read('spec/fixtures/' << dir << fixture_name << '.yml')
+    f = File.read('spec/fixtures/' << fixture_name << '.yml')
+    #, '"99*/*"'
+    if RUBY_VERSION =~ /2.0.\d+/
+      f.gsub(/("\*\/\*")/, '\'*/*\'')
+    elsif RUBY_VERSION =~ /2.1.0/
+      f.gsub(/string: '(.+)'/) do |found|
+        found.gsub('"', '\"').gsub('\'', '"')
+      end
+    else
+      f
+    end
   end
 end
